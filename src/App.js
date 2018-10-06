44fd9a5c09de6ae476c39ec407018abf73fef39f
Tucker <trblackw@ncsu.edu>
2018-10-06 15:46:40 -0400

    playing around with app styles; included moviedb logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { render } from "react-dom";
+// import { ReactDOM } from 'react-dom';
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import Nav from "./components/Nav";
+import GenreSelection from "./components/GenreSelection";
+import TrendingSelection from "./components/TrendingSelection";
+import Search from "./components/Search";
+import PeopleList from "./components/PeopleList";
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>WOOOHOOO</div>;
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route path="/genres" component={GenreSelection} />
+            <Route path="/people" component={PeopleList} />
+            <Route path="/trending" component={TrendingSelection} />
+            <Route path="/search" component={Search} />
+            <Route path="/:id" component={MovieDetail} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
+
+export default App;
 
 render(<App />, document.getElementById("root"));
