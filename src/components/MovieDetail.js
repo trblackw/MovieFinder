1c297928fc62bd7aed0525d98980e5556262adba
Tucker <trblackw@ncsu.edu>
2018-10-06 13:39:28 -0400

    created components to generate list of all movies, as well as detail component for specific movie

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,191 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Poster } from "./Movie";
+import Overdrive from "react-overdrive";
+import PropTypes from "prop-types";
+const API_KEY = process.env.API_KEY;
+
+class MovieDetail extends Component {
+  state = {
+    movie: {},
+    genre: [],
+    review: []
+  };
+
+  //helper functions
+  formatBudget = num => String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  formatRuntime = time => {
+    let hours = Math.floor(time / 60);
+    let minutes = time / 60;
+    return `${hours} hours ${((minutes - hours) * 60).toFixed(0)} minutes`;
+  };
+
+  async componentDidMount() {
+    try {
+      const movieRes = await fetch(
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }?api_key=${API_KEY}&language=en-US`
+      );
+      const reviewRes = await fetch(
+        `http://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }/reviews?api_key=${API_KEY}`
+      );
+      const movie = await movieRes.json();
+      const review = await reviewRes.json();
+
+      this.setState({
+        movie,
+        genre: movie.genres,
+        review: review.results
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    const POSTER_PATH = "http://image.tmdb.org/t/p/w185";
+    const BACKDROP_PATH = "http://image.tmdb.org/t/p/w1280";
+
+    const { movie, genre, review } = this.state;
+    return (
+      <DetailWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <DetailInfo>
+          <Overdrive id={String(movie.id)}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt="poster" />
+          </Overdrive>
+          <div id="info">
+            <h1>{movie.title}</h1>
+            <div id="infoAttr">
+              <p className="first">{movie.release_date}</p>
+              <p>
+                {movie.vote_average}
+                /10
+              </p>
+              {movie.budget && (
+                <p>Budget: ${this.formatBudget(movie.budget)}</p>
+              )}
+
+              <p>{this.formatRuntime(movie.runtime)}</p>
+            </div>
+            <GenreList>
+              {genre.map(item => (
+                <li key={item.id}>{item.name}</li>
+              ))}
+            </GenreList>
+            <p>{movie.overview}</p>
+          </div>
+        </DetailInfo>
+        {review.length && (
+          <ReviewSection>
+            <h1>Reviews</h1>
+            {review.map(rev => (
+              <div key={rev.id}>
+                <h4>{rev.author}</h4>
+                <p id="review">
+                  <em>{`"${rev.content}"`}</em>
+                </p>
+              </div>
+            ))}
+          </ReviewSection>
+        )}
+      </DetailWrapper>
+    );
+  }
+}
+//isRequired affected by initial emptiness of props
+MovieDetail.propTypes = {
+  movie: PropTypes.object,
+  genre: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default MovieDetail;
+
+const DetailWrapper = styled.div`
+  position: relative;
+  padding-top: 60vh;
+  background: url(${props => props.backdrop}) no-repeat;
+  background-position: relative;
+  object-fit: cover;
+  justify-content: center;
+`;
+
+const DetailInfo = styled.div`
+  background: rgb(245, 245, 245, 0.7);
+  text-align: left;
+  line-height: 1.9em;
+  padding: 2rem 10%;
+  display: flex;
+  margin-top: 1.9em;
+  font-size: 1.2em;
+  div#info {
+    margin-left: 20px;
+  }
+  div#infoAttr {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: center;
+    margin: 1em auto 1em 0;
+    color: hsl(0, 100%, 59%);
+  }
+  div#infoAttr > p:not(.first) {
+    display: inline-block;
+    margin-left: 1.5em;
+  }
+
+  div#infoAttr > p:hover {
+    color: hsl(0, 100%, 72%);
+    cursor: pointer;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
+
+const GenreList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  li {
+    list-style: none;
+    font-style: italic;
+    font-size: 0.8em;
+    font-weight: 700;
+    margin-left: 1em;
+    color: dodgerblue;
+  }
+`;
+
+const ReviewSection = styled.div`
+  padding: 2em;
+  margin: 0 auto;
+  background: black;
+  color: whitesmoke;
+  div {
+    margin-bottom: 1.5em;
+    padding-bottom: 1.5em;
+    border-bottom: 1px solid hsl(0, 100%, 59%);
+    line-height: 1.6em;
+  }
+  h1 {
+    text-align: center;
+    font-size: 2.5em;
+    margin-bottom: 1em;
+  }
+  h4 {
+    font-size: 2em;
+    font-weight: 600;
+    margin-bottom: 0.5em;
+  }
+  #review {
+    text-indent: 0.7em;
+    font-family: "Open sans", sans-serif;
+  }
+`;
---
+++ src/components/MovieList.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import Movie from "./Movie";
+// import { NavLink } from "react-router-dom";
+
+const API_KEY = process.env.API_KEY;
+const pageOne = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const pageTwo = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2`;
+const pageThree = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=3`;
+
+class MovieList extends Component {
+  state = {
+    pages: [],
+    activePage: []
+  };
+
+  mapMovieData = page =>
+    page.map(movie => (
+      <Movie key={movie.id} movie={movie} img={movie.poster_path} />
+    ));
+
+  selectPage = e => {
+    this.setState({
+      activePage: e.target.value
+    });
+    console.log(this.state.activePage);
+  };
+
+  async componentDidMount() {
+    try {
+      const movieRes = await Promise.all([
+        fetch(pageOne),
+        fetch(pageTwo),
+        fetch(pageThree)
+      ]).then(responses => Promise.all(responses.map(res => res.json())));
+      const pages = movieRes.map(page => page.results);
+
+      this.setState({
+        pages,
+        activePage: pages[0]
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    const [page1, page2, page3] = this.state.pages;
+    return (
+      <div>
+        <Pages>
+          <li
+            onClick={() => this.setState({ activePage: page1 })}
+            className="page"
+          >
+            Page 1
+          </li>
+          <li
+            onClick={() => this.setState({ activePage: page2 })}
+            className="page"
+          >
+            Page 2
+          </li>
+          <li
+            onClick={() => this.setState({ activePage: page3 })}
+            className="page"
+          >
+            Page 3
+          </li>
+        </Pages>
+        <MovieGrid>
+          {this.state.activePage.length ? (
+            this.mapMovieData(this.state.activePage)
+          ) : (
+            <div>LOADING...</div>
+          )}
+        </MovieGrid>
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  pages: PropTypes.arrayOf(PropTypes.array),
+  activePage: PropTypes.array
+};
+
+export default MovieList;
+
+export const MovieGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-gap: 1em;
+  padding: 2em;
+  margin: 0 auto;
+  overflow-x: scroll;
+`;
+
+const Pages = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  width: auto;
+  margin: 0 0.5em;
+
+  li {
+    font-size: 0.8em;
+    list-style: none;
+    margin-right: 0.5em;
+    color: whitesmoke;
+  }
+  li:hover,
+  li:active {
+    color: red;
+    cursor: pointer;
+  }
+`;
