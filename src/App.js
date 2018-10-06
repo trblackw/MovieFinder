098ab09aad05e13a30160db2929b11e9bf4ece3a
Tucker <trblackw@ncsu.edu>
2018-10-06 01:08:11 -0400

    initial commit; babel, eslint, prettier, parcel configured

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+  render() {
+    return <div>WOOOHOOO</div>;
+  }
+}
+
+render(<App />, document.getElementById("root"));
