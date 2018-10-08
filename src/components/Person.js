6119dd3565d6c426d57aa6eff00d29ae13b224d7
Tucker <trblackw@ncsu.edu>
2018-10-08 11:03:59 -0400

    Linked each person's movies to their MovieDetails component

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,11 +1,12 @@
 import React from "react";
-// import styled from "styled-components";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const POSTER_PATH = "https://image.tmdb.org/t/p/w185";
 
 const PersonMovies = ({ movies }) => {
   return (
-    <details>
+    <Details>
       <summary
         style={{
           color: "whitesmoke"
@@ -14,14 +15,11 @@ const PersonMovies = ({ movies }) => {
         Movies
       </summary>
       {movies.map(movie => (
-        <p
-          style={{ color: "whitesmoke", fontWeight: "lighter" }}
-          key={movie.id}
-        >
-          {movie.title}
-        </p>
+        <Link key={movie.id} to={`/${movie.id}`}>
+          <P>{movie.title}</P>
+        </Link>
       ))}
-    </details>
+    </Details>
   );
 };
 
@@ -30,13 +28,13 @@ const Person = ({ name, movies, image }) => {
     <div>
       <p
         style={{
-          color: "red",
+          color: "hsl(196, 82%, 60%)",
           fontWeight: "bolder"
         }}
       >
         {name}
       </p>
-      <img src={`${POSTER_PATH}${image}`} alt={{name}} />
+      <img src={`${POSTER_PATH}${image}`} alt={{ name }} />
 
       <PersonMovies movies={movies} />
     </div>
@@ -44,3 +42,30 @@ const Person = ({ name, movies, image }) => {
 };
 
 export default Person;
+
+const P = styled.p`
+  color: whitesmoke;
+  text-decoration: none;
+  font-weight: lighter;
+  font-style: italic;
+  font-size: 0.9em;
+  &:hover {
+    color: hsl(196, 82%, 60%);
+  }
+`;
+
+const Details = styled.details`
+  summary::-webkit-details-marker {
+    display: none;
+  }
+  summary:before {
+    display: inline-block;
+    width: 18px;
+    height: 18px;
+    margin-right: 8px;
+    content: "";
+    background-image: url(http://cdn.onlinewebfonts.com/svg/img_113936.png);
+    background: no-repeat:
+    background-position: 0 0;
+  }
+`;
