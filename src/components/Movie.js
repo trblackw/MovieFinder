a6f19e1d367c6eb043f24e2ea9be41f5e06eb0c7
Tucker <trblackw@ncsu.edu>
2018-10-12 14:17:08 -0400

    updated proptypes; still dealing with bizarre tv show linking issue

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -20,18 +20,19 @@ const Movie = ({ movie, show }) =>
       </Overdrive>
     </Link>
   );
-// const Movie = ({ movie }) => (
-//   <Link to={`/${movie.id}`}>
-//     <Overdrive id={String(movie.id)}>
-//          <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-//     </Overdrive>
-//   </Link>
-// );
+
 
 Movie.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string
-  }).isRequired
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    poster_path: PropTypes.string.isRequired
+  }),
+  show: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    poster_path: PropTypes.string.isRequired
+  })
 };
 
 export default Movie;
