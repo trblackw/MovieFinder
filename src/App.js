b280b8d895a86c49d62b01f58f3fdf7fbd180d55
Tucker <trblackw@ncsu.edu>
2018-10-14 18:32:32 -0400

    created redux boilerplate

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Fragment } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { render } from "react-dom";
-import MovieList from "./components/MovieList";
-import MovieDetail from "./components/MovieDetail";
+import MovieList from "./components/movies/MovieList";
+import MovieDetail from "./components/movies/MovieDetail";
 import Nav from "./components/Nav";
-import GenreSelection from "./components/GenreSelection";
-import TrendingSelection from "./components/TrendingSelection";
-import Search from "./components/Search";
-import PeopleList from "./components/PeopleList";
+import GenreSelection from "./components/genres/GenreSelection";
+import TrendingSelection from "./components/trending/TrendingSelection";
+import Search from "./components/search/Search";
+import PeopleList from "./components/people/PeopleList";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
 const App = () => (
   <Router>
@@ -28,4 +30,9 @@ const App = () => (
 
 export default App;
 
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+const placeholder = () => "temporary";
+
+const rootReducer = combineReducers({
+  placeholder
+});
+
+export default rootReducer;
