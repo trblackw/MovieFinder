877bd2fd1c859ffe290912d4379e5cbe544988fc
Tucker <trblackw@ncsu.edu>
2018-10-19 14:26:58 -0400

    move component state to redux store & fetch call to actions

--- src/components/trending/TrendingSelection.js
+++ src/components/trending/TrendingSelection.js
@@ -1,56 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Movie from "../movies/Movie";
 import { MovieGrid } from "../movies/MovieList";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { fetchTrending } from "./trending_actions";
 
 class TrendingSelection extends Component {
-  state = {
-    //  trending: [],
-    trendingMovies: [],
-    trendingTVShows: []
-  };
-
-  async componentDidMount() {
-    try {
-      const trendingRes = await fetch(
-        "https://api.themoviedb.org/3/trending/all/day?api_key=ba1ed06e4a2885f1ceb3219f8f940de6"
-      );
-      const trending = await trendingRes.json();
-      const trendingMovies = trending.results.filter(
-        item => !item.original_name
-      );
-      const trendingTVShows = trending.results.filter(
-        item => item.original_name
-      );
-
-      this.setState({
-        //   trending: trending.results
-        trendingMovies,
-        trendingTVShows
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  componentDidMount() {
+    const { fetchTrending } = this.props;
+    fetchTrending();
   }
 
   render() {
-    const { trendingMovies, trendingTVShows } = this.state;
+    const { movies, shows } = this.props;
     return (
-      <div>
-        <h2 style={{ color: "whitesmoke", margin: '0 1em'}}>Movies</h2>
+      <Fragment>
+        <h2 style={{ color: "whitesmoke", margin: "0 1em" }}>Movies</h2>
         <MovieGrid>
-          {trendingMovies.map(movie => (
+          {movies.map(movie => (
             <Movie key={movie.id} title={movie.title} movie={movie} />
           ))}
         </MovieGrid>
-        <h2 style={{ color: "whitesmoke", margin: '0 1em' }}>TV Shows</h2>
+        <h2 style={{ color: "whitesmoke", margin: "0 1em" }}>TV Shows</h2>
         <MovieGrid>
-          {trendingTVShows.map(show => (
+          {shows.map(show => (
             <Movie key={show.id} title={show.name} show={show} />
           ))}
         </MovieGrid>
-      </div>
+      </Fragment>
     );
   }
 }
 
-export default TrendingSelection;
+const mapStateToProps = state => ({
+  movies: state.TrendingReducer.trendingMovies,
+  shows: state.TrendingReducer.trendingTVShows
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ fetchTrending }, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TrendingSelection);
