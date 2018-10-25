cfdb99cc143af80b446c43498d9c3096d26d797e
Tucker <trblackw@ncsu.edu>
2018-10-25 14:44:22 -0400

    increased movie page count

--- src/components/movies/movies_actions.js
+++ src/components/movies/movies_actions.js
@@ -6,13 +6,17 @@ export const fetchMovies = () => {
   const pageOne = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
   const pageTwo = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2`;
   const pageThree = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=3`;
+  const pageFour = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=4`;
+  const pageFive = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=5`;
   return async dispatch => {
     const movieRes = await Promise.all([
       fetch(pageOne),
       fetch(pageTwo),
-      fetch(pageThree)
+      fetch(pageThree),
+      fetch(pageFour),
+      fetch(pageFive)
     ]).then(responses => Promise.all(responses.map(res => res.json())));
-    const pages = movieRes.map(page => page.results);
+     const pages = movieRes.map(page => page.results);
     return dispatch({
       type: FETCH_MOVIES,
       pages
