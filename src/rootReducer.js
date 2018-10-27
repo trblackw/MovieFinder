84d5a2e4b5dab209dbf595ea93d6bc316e3e9046
Tucker <trblackw@ncsu.edu>
2018-10-27 13:34:15 -0400

    removed unused search reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from "redux";
 import MoviesReducer from "./components/movies/movies_reducer";
 import PeopleReducer from "./components/people/people_reducer";
 import TrendingReducer from "./components/trending/trending_reducer";
-import SearchReducer from "./components/search/search_reducer";
 import GenresReducer from "./components/genres/genre_reducer";
 
 const rootReducer = combineReducers({
   MoviesReducer,
   PeopleReducer,
   TrendingReducer,
-  SearchReducer,
   GenresReducer
 });
 
